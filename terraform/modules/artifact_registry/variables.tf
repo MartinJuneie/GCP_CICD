455b68a16e573a8993f5098669dcfa42ecc530ad
Martin Joseph Juneie <martinjuneie@gmail.com>
2026-08-28 18:45:57 +0530

    Add missing variables for artifact registry module

---
+++ terraform/modules/artifact_registry/variables.tf
@@ -0,0 +1,24 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP Region for Artifact Registry"
+  type        = string
+}
+
+variable "region_short" {
+  description = "Shorthand code for the region (e.g., usc1)"
+  type        = string
+}
+
+variable "repository_id" {
+  description = "Repository name identifier in Artifact Registry"
+  type        = string
+}
+
+variable "description" {
+  description = "Description of the repository"
+  type        = string
+}
